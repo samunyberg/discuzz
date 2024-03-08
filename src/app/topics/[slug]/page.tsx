3e05b69c5cefc0dc92a7719ae9cd198e224815a3
samunyberg <samu.nyberg@gmail.com>
2024-03-08 19:52:10 +0200

    Create page routes

---
+++ src/app/topics/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowTopicPage = () => {
+  return (
+    <div>ShowTopicPage</div>
+  )
+}
+
+export default ShowTopicPage
---
+++ src/app/topics/[slug]/posts/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowPostPage = () => {
+  return (
+    <div>ShowPostPage</div>
+  )
+}
+
+export default ShowPostPage
