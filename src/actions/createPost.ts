da96d5b53d524d20a56abff10ac84015a92ff6b2
samunyberg <samu.nyberg@gmail.com>
2024-03-08 20:18:45 +0200

    Identify the actions where data has to be revalidated

--- src/actions/createPost.ts
+++ src/actions/createPost.ts
@@ -1,3 +1,5 @@
 "use server";
 
-export async function createPost() {}
+export async function createPost() {
+    // TODO: Revalidate the showTopic page after creating a post to avoid caching issues.
+}
--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -1,3 +1,5 @@
 "use server";
 
-export async function createTopic() {}
+export async function createTopic() {
+  // TODO: Revalidate the homepage after creating a topic to avoid caching issues.
+}
