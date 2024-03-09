aa56153fc27d72c5f4857c85d42d81bdba006190
samunyberg <samu.nyberg@gmail.com>
2024-03-09 16:54:08 +0200

    Remove simulated delay from createTopic action

--- src/actions/createTopic.ts
+++ src/actions/createTopic.ts
@@ -31,8 +31,6 @@ export async function createTopic(
   formsState: CreateTopicFormState,
   formData: FormData
 ): Promise<CreateTopicFormState> {
-  await new Promise((resolve) => setTimeout(resolve, 2500));
-
   const session = await auth();
   if (!session || !session.user)
     return {
